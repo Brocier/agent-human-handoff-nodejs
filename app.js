dc63e6f5356a08d495b8f1228227db67c1c26150
Brocier <jschoe04@gmail.com>
2018-07-24 11:54:14 -0400

    DialogFlow up and running with dotenv

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-// Copyright 2017, Google, Inc.
-// Licensed under the Apache License, Version 2.0 (the 'License');
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Copyright 2017, Google, Inc. Licensed under the Apache License, Version 2.0
+// (the 'License'); you may not use this file except in compliance with the
+// License. You may obtain a copy of the License at
 //
 //    http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an 'AS IS' BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// Load third party dependencies
+// distributed under the License is distributed on an 'AS IS' BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License. Load third party dependencies
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+
+require('dotenv').config()
 
 // Load our custom classes
 const CustomerStore = require('./customerStore.js');
@@ -22,21 +21,21 @@ const MessageRouter = require('./messageRouter.js');
 
 // Grab the service account credentials path from an environment variable
 const keyPath = process.env.DF_SERVICE_ACCOUNT_PATH;
-if(!keyPath) {
-  console.log('You need to specify a path to a service account keypair in environment variable DF_SERVICE_ACCOUNT_PATH. See README.md for details.');
+if (!keyPath) {
+  console.log('You need to specify a path to a service account keypair in environment variable ' +
+      'DF_SERVICE_ACCOUNT_PATH. See README.md for details.');
   process.exit(1);
 }
 
 // Load and instantiate the Dialogflow client library
-const { SessionsClient } = require('dialogflow');
-const dialogflowClient = new SessionsClient({
-  keyFilename: keyPath
-})
+const {SessionsClient} = require('dialogflow');
+const dialogflowClient = new SessionsClient({keyFilename: keyPath})
 
 // Grab the Dialogflow project ID from an environment variable
 const projectId = process.env.DF_PROJECT_ID;
-if(!projectId) {
-  console.log('You need to specify a project ID in the environment variable DF_PROJECT_ID. See README.md for details.');
+if (!projectId) {
+  console.log('You need to specify a project ID in the environment variable DF_PROJECT_ID. See ' +
+      'README.md for details.');
   process.exit(1);
 }
 
